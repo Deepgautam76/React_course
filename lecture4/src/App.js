741c588b77270d27cd165d81ac03e8d79856c9db
Kamal <kamaldeep734833@gmail.com>
2025-02-21 19:04:16 +0530

    add the react router

--- lecture4/src/App.js
+++ lecture4/src/App.js
@@ -2,17 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
+import About from "./components/About";
+import Error from "./components/Error";
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router";
+import Contect from "./components/Contect";
 
 // Main component
 const App = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet/>
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path:"/contect",
+        element:<Contect />
+      }
+    ],
+    errorElement: <Error />
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+// root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
