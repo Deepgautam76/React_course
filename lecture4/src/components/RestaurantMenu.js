e1ad3cc2b1b7bdef8001ba15a7d5ce1a0c1fe7d1
Kamal <kamaldeep734833@gmail.com>
2025-02-22 22:33:31 +0530

    add restaurantMenu components

---
+++ lecture4/src/components/RestaurantMenu.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+
+import { useParams } from "react-router";
+import Shimmerui from "./Shimmerui";
+import { MENU_API } from "../utils/constant";
+
+
+const RestaurantMenu = () => {
+  const [resMenu, setResMenu] = useState(null);
+  const param = useParams();
+  const resId=param.id;
+  console.log(resId);
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const response = await fetch(MENU_API + param.id);
+    const data = await response.json();
+    // console.log(data);
+    const menu=data?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR.cards;
+    // console.log(menuCards);
+    const menuCards=menu[1].card?.card?.itemCards;
+    console.log(menu[1].card?.card?.itemCards);
+    setResMenu(menuCards);
+  };
+
+  if (resMenu === null) return <Shimmerui />;
+  return (
+    <div>
+      {
+        resMenu.map((menu)=>{
+          return(
+            <div className="menu" key={menu.card.info.id}>
+              <h1>{menu.card.info.name}</h1>
+              <h3>{menu.card.info.defaultprice}</h3>
+              <p>{menu.card.info.description}</p>
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+};
+
+export default RestaurantMenu;
