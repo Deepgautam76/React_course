ca54d08f5978b1efd5bfb2f483ad16d4f776b86c
Kamal <kamaldeep734833@gmail.com>
2025-02-24 23:22:41 +0530

    Add the User Component

---
+++ lecture4/src/components/User.js
@@ -0,0 +1,12 @@
+const User=()=>{
+
+    return (
+        <div className="user-card">
+            <h1>Name: Kamal deep</h1>
+            <h3>Location: Noida</h3>
+            <h4>Contect: @gamil.com</h4>
+        </div>
+    )
+}
+
+export default User;
