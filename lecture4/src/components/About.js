be689a20c04e3f221b0ea72f788b30718310488e
Kamal <kamaldeep734833@gmail.com>
2025-02-25 22:27:21 +0530

    Update the About Component

--- lecture4/src/components/About.js
+++ lecture4/src/components/About.js
@@ -1,8 +1,11 @@
 import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
 const About = () => {
   return (
     <div>
-      <h1>Hello from about</h1>
+      <h1>About</h1>
+      <UserClass name="Deep" location="Noida (NCR) " />
     </div>
   );
 };
