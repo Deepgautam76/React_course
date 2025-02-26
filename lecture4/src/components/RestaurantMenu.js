1d34e56d73905f0a69c8cc640256cb20e6391088
Kamal <kamaldeep734833@gmail.com>
2025-02-26 14:58:30 +0530

    Refactor code and use the custom hooks

--- lecture4/src/components/RestaurantMenu.js
+++ lecture4/src/components/RestaurantMenu.js
@@ -2,45 +2,36 @@ import React, { useEffect, useState } from "react";
 
 import { useParams } from "react-router";
 import Shimmerui from "./Shimmerui";
-import { CDN_URL, MENU_API } from "../utils/constant";
+import { CDN_URL} from "../utils/constant";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resMenu, setResMenu] = useState(null);
   const param = useParams();
   const resId = param.id;
-  console.log(resId);
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const response = await fetch(MENU_API + resId);
-    const data = await response.json();
-    // console.log("data")
-    // console.log(data);
-    const menu = data?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR.cards;
-    // console.log(menuCards);
-    const menuCards = menu[1].card?.card?.itemCards;
-    console.log(menuCards);
-    setResMenu(menuCards);
-  };
+  // Custom hooks for fetching the restaurant-menu data
+  const resMenu = useRestaurantMenu(resId);
 
   if (resMenu === null) return <Shimmerui />;
   return (
     <div className="menu-cards">
       {resMenu.map((menu) => {
+        const { id, name, defaultprice, description, imageId } =
+          menu?.card?.info;
         return (
-          <div className="menu-container" key={menu?.card?.info?.id}>
-            <div className="menu-detailed">
-              <h1>{menu?.card?.info?.name}</h1>
-              <h3>{menu?.card?.info?.defaultprice}</h3>
-              <p>{menu?.card?.info?.description}</p>
+          <>
+            <div className="menu-container" key={id}>
+              <div className="menu-detailed">
+                <h1>{name}</h1>
+                <h3>{defaultprice}</h3>
+                <p>{description}</p>
+              </div>
+              <div className="img-container">
+                <img src={CDN_URL + imageId} alt="resImage" />
+              </div>
             </div>
-            <div className="img-container">
-              <img src={CDN_URL + menu?.card?.info?.imageId} alt="resImage" />
-            </div>
-          </div>
+            <div className="card-seprator"></div>
+          </>
         );
       })}
     </div>
