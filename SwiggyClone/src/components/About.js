a19c8472376a7c5702dc0757094a65142fca0cb4
Kamal <kamaldeep734833@gmail.com>
2025-03-02 23:16:17 +0530

    modify the about.js file

--- SwiggyClone/src/components/About.js
+++ SwiggyClone/src/components/About.js
@@ -3,7 +3,8 @@ import User from "./User";
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <h3>It's build by the developer kamal deep</h3>
+      <h3>This is GitHub profile of the kamal deep</h3>
       <User name="Deep" location="Noida (NCR)" />
     </div>
   );
