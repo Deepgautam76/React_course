be0fed98aa8dc4154740597cf1ea3bf76cb353e0
Kamal <kamaldeep734833@gmail.com>
2025-03-08 00:19:06 +0530

    user the ContextAPI

---
+++ SwiggyClone/src/utils/UserContext.js
@@ -0,0 +1,21 @@
+import { createContext } from "react";
+
+/**
+ * This is the way to create  
+ * the contextProvide using 
+ * keyword - "createContext"
+ * -- And if you want to access this context use this --
+ *   like (const data=useContext("enter the context name")
+ *    const data=useContext(UserContext)
+ *  -- if you had old class based components use this way to acess the context--
+ *    <UserContext.Consumer>
+ *     {("receive data here in the form of cb")=>}
+ *    </UserContext.Consumer>
+ */
+
+const UserContext = createContext({
+  Name: "kamaldeep",
+  age:23
+});
+
+export default UserContext;
