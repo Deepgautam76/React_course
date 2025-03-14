aec1caf938b3d41b56a3a7db02ad18d85966eb00
Kamal <kamaldeep734833@gmail.com>
2025-03-14 23:32:42 +0530

    refactor the useRestaurantMenu

--- SwiggyClone/src/utils/useRestaurantMenu.js
+++ SwiggyClone/src/utils/useRestaurantMenu.js
@@ -12,12 +12,9 @@ const useRestaurantMenu = (resId) => {
     try {
       const response = await fetch(MENU_API + resId);
       const data = await response.json();
-      // const menu =
-      //   data?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
-      // setResMenu(menu);
-      setResMenu(data)
+      setResMenu(data);
     } catch (error) {
-      console.log("Error encounter frome useRestaurantMenu:" + error);
+      console.log("Error encounter from useRestaurantMenu:" + error);
     }
   };
   return resMenu;
