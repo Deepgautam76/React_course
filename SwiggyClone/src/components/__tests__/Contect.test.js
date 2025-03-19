760b4a70b9fb7c688443c343b1c3c66e5546b7fc
Kamal <kamaldeep734833@gmail.com>
2025-03-19 22:26:56 +0530

    using the dercribe function to divide test into category

--- SwiggyClone/src/components/__tests__/Contect.test.js
+++ SwiggyClone/src/components/__tests__/Contect.test.js
@@ -2,20 +2,34 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Contect from "../header/Contect";
 
-test("should load heading", () => {
-  render(<Contect />);
-  const heading = screen.getByRole("heading");
+/**
+ * Using describe to group the test cases
+ * describe is a function that takes two arguments
+ * 1. Name of the test group  (String)
+ * 2. Callback function  (Function) that contains the test cases
+ */
 
-  //Assertion
-  expect(heading).toBeInTheDocument();
-});
+describe("Contect Component", () => {
+  test("should load heading", () => {
+    render(<Contect />);
+    const heading = screen.getByRole("heading");
+    //Assertion
+    expect(heading).toBeInTheDocument();
+  });
+  
+  // You can also write testcases unsing it() function
+  it("should load heading", () => {
+    render(<Contect />);
+    const heading = screen.getByRole("heading");
+    //Assertion
+    expect(heading).toBeInTheDocument();
+  });
 
-test("should load paragraph", () => {
-  render(<Contect />);
-
-  //Querying
-  const paragraph = screen.getAllByRole("paragraph");
-
-  //Assertion
-  expect(paragraph[0]).toBeInTheDocument();
+  test("should load paragraph", () => {
+    render(<Contect />);
+    //Querying
+    const paragraph = screen.getAllByRole("paragraph");
+    //Assertion
+    expect(paragraph[0]).toBeInTheDocument();
+  });
 });
--- SwiggyClone/src/components/header/About.js
+++ SwiggyClone/src/components/header/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div className="h-lvh flex flex-col m-4 min-h-screen">
       <div className="flex flex-col px-8 ">
         <h3 className="text-3xl font-semibold text-green-400">
-          It's build by the developer Kamal Deep
+          It's Build By Developer Kamal Deep
         </h3>
       </div>
       <div>
