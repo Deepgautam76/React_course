0b0835ef094973cf01fb42a9be15c1e7cf698cbc
Kamal <kamaldeep734833@gmail.com>
2025-03-21 22:46:56 +0530

    write test case for login logout button

--- SwiggyClone/src/components/__tests__/header.test.js
+++ SwiggyClone/src/components/__tests__/header.test.js
@@ -1,3 +1,13 @@
-test('should we header', () => {
-    
-})
+import { render, screen } from "@testing-library/dom";
+import "@testing-library/jest-dom";
+
+import Header from "../header/Header";
+
+describe("All testcase for header component", () => {
+  test("should we header login logout button", () => {
+    render(<Header />);
+    const button = screen.getByRole("button");
+    //Assert
+    expect(button).toBeInTheDocument();
+  });
+});
