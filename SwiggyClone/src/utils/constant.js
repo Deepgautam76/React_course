11b9d6ac2e87448989879d067f98cceba23a0e24
Kamal <kamaldeep734833@gmail.com>
2025-04-07 20:35:07 +0530

    add the api params

--- SwiggyClone/src/utils/constant.js
+++ SwiggyClone/src/utils/constant.js
@@ -7,5 +7,16 @@ export const LOGO_URL =
 export const SWIGGY_API =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630&collection=83631&tags=layout_CCS_Pizza&sortBy=&filters=&type=rcv2&offset=0";
 
+export const BASE_URL = "https://www.swiggy.com/dapi/restaurants/list/v5";
+
+export const PARAMS = new URLSearchParams({
+  lat: "28.65200",
+  lng: "77.16630",
+  collection: "83631",
+  tags: "layout_CCS_Pizza",
+  sortBy: "",
+  filters: "",
+  type: "rcv2",
+});
 export const MENU_API =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65200&lng=77.16630&restaurantId=";
