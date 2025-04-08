c65f597367656b959822d4bfdf7700f4674d6506
Kamal <kamaldeep734833@gmail.com>
2025-04-08 19:41:02 +0530

    Refactore the code add infinite scrolling

--- SwiggyClone/src/components/Body.js
+++ SwiggyClone/src/components/Body.js
@@ -1,118 +1,70 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router";
-import RestaurantCart, { withPromotedlevel } from "./restaurant/RestaurantCart";
+import RestaurantCart from "./restaurant/RestaurantCart";
 import Shimmerui from "./Shimmerui";
-import { SWIGGY_API } from "../utils/constant";
+import useInfiniteRestaurants from "../utils/useInfiniteRestaurant";
 
 const Body = () => {
-  const [restaurantData, setRestaurantData] = useState([]);
-  const [filterRestaurant, setFilterRestaurant] = useState([]);
   const [searchText, setSearchText] = useState("");
 
-  // Promoted level component object
-  const PromotedLavel = withPromotedlevel(RestaurantCart);
-
-  /**
-   * Use Effiect render after the whole page rendered
-   * Im using this first render the page then call the API for dynamic data
-   */
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  /**
-   * Fetch Swigqy API dynamic data
-   */
-  const fetchData = async () => {
-    try {
-      const response = await fetch(SWIGGY_API);
-      const Data = await response.json();
-      const restaurantCarts = Data?.data?.cards;
-      const [card1, card2, card3, ...resCards] = restaurantCarts;
-      setRestaurantData(resCards);
-      setFilterRestaurant(resCards);
-    } catch (error) {
-      console.log("Error encounter restaurant card fetching:" + error);
-    }
-  };
-
-  /**
-   * Filter restaurant card based on rating
-   */
-  const filterData = () => {
-    const filtred = restaurantData.filter(
-      (res) => res?.card?.card?.info?.avgRating > 3.9
-    );
-    setFilterRestaurant(filtred);
-  };
-
-  /**
-   * Reset all filter that apply by user
-   */
-  const reset = () => {
-    setFilterRestaurant(restaurantData);
-  };
+  const {
+    loaderRef,
+    restaurantData,
+    filterRestaurant,
+    loading,
+    filterTopRated,
+    resetFilter,
+    searchRestaurant,
+  } = useInfiniteRestaurants();
 
   return restaurantData.length === 0 ? (
     <Shimmerui />
   ) : (
     <div className="container">
-      <div className="flex flex-wrap m-auto p-2  border-b-1 w-9/12">
+      <div className="flex flex-wrap m-auto p-2 border-b-1 w-9/12">
         <button
-          className="m-1 py-2 px-3 cursor-pointer bg-gray-400 border-0 rounded-xl hover:bg-gray-500"
-          onClick={filterData}
+          className="m-1 py-2 px-3 bg-gray-400 rounded-xl hover:bg-gray-500"
+          onClick={filterTopRated}
         >
           Top Rated
         </button>
         <button
-          className="m-1 py-2 px-3 cursor-pointer bg-gray-400 border-0 rounded-xl hover:bg-gray-500"
-          onClick={reset}
+          className="m-1 py-2 px-3 bg-gray-400 rounded-xl hover:bg-gray-500"
+          onClick={resetFilter}
         >
           Reset filter
         </button>
         <input
           type="text"
-          className="m-1 mx-3 py-2 px-3 cursor-pointer border-2 rounded-2xl"
+          className="m-1 mx-3 py-2 px-3 border-2 rounded-2xl"
           value={searchText}
           onChange={(e) => setSearchText(e.target.value)}
           placeholder="Search foods"
         />
         <button
-          className="m-1 mx-3 py-2 px-3 cursor-pointer bg-blue-700 font-bold rounded-2xl hover:bg-blue-800"
+          className="m-1 mx-3 py-2 px-3 bg-blue-700 font-bold rounded-2xl hover:bg-blue-800"
           onClick={() => {
-            /**
-             * Filter the restaurant crad based on the search text entered
-             * SearchText is the state which is holding the value of the search input
-             * Here I am filtering the data based on the search text
-             */
-            const filterRestaurantData = restaurantData.filter((res) =>
-              res?.card?.card?.info?.name
-                .toLowerCase()
-                .includes(searchText.toLowerCase())
-            );
-            if (filterRestaurantData.length != 0) {
-              setFilterRestaurant(filterRestaurantData);
-            }
+            searchRestaurant(searchText);
             setSearchText("");
           }}
         >
           Search
         </button>
       </div>
+
       <div className="container flex w-9/12 m-auto flex-wrap">
         {filterRestaurant.map((item) => (
           <NavLink
             key={item?.card?.card?.info?.id}
             to={`/restaurant/${item?.card?.card?.info?.id}`}
           >
-            {item?.card?.card?.info?.promoted ? (
-              <PromotedLavel cardData={item?.card?.card} />
-            ) : (
-              <RestaurantCart cardData={item?.card?.card} />
-            )}
+            <RestaurantCart cardData={item?.card?.card} />
           </NavLink>
         ))}
       </div>
+
+      {loading && <p className="text-center mt-4">Loading more...</p>}
+      <div ref={loaderRef} className="h-10"></div>
     </div>
   );
 };
